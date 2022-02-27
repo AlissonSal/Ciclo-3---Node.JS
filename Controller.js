0308bfd910ba6ed87628ece13616e2ef836369cc
AlissonSal <alisson000silva@gmail.com>
2022-02-27 00:06:02 -0300

    atualização 27-02

--- Controller.js
+++ Controller.js
@@ -13,41 +13,19 @@ let cliente = models.Cliente;
 let servico = models.Servico;
 let pedido = models.Pedido;
 let itempedido = models.ItemPedido;
+let compra = models.Compra;
+let produto = models.Produto;
+let itemcompra = models.ItemCompra;
 
-app.get('/', function(rep,res){
+app.get('/', function(req,res){
     res.send('Oi Alisson')
 });
 
-app.post('/servicos', async(rep,res)=>{
-    await servico.create(
-        rep.body
-        // nome: "Delphi",
-        // descricao: "Manutenção e suporte a sustemas legados em Delphi",
-        // createAt: new Date(),
-        // uptadeAt: new Date()
-    ).then(function(){
-        return res.json({
-            error: false,
-            message: "Serviço criado com sucesso!"        
-        })
-    }).catch(function(erro){
-        return res.status(400).json({
-            error: true,
-            message: "Erro no cadastro do serviço"
-        })
-    });
-    // res.send('Serviço criado com sucesso!');
-});
+// inserir
 
-app.post('/clientes', async(rep,res)=>{
+app.post('/clientes', async(req,res)=>{
     await cliente.create(
-        rep.body
-        // nome: "Alisson Henrique",
-        // endereco: "Rua Mandaguari, 182",
-        // cidade:"Maringá",
-        // uf: "PR",
-        // nascimento: "1992-19/12",
-        // clienteDesde: "2022-16/02"
+        req.body
     ).then(function(){
         return res.json({
             error: false,
@@ -59,14 +37,27 @@ app.post('/clientes', async(rep,res)=>{
             message: "Erro no cadastro do cliente"
         })
     });
-    // res.send('Cliente criado com sucesso!');
 })
 
-app.post('/pedidos', async(rep,res)=>{
+app.post('/servicos', async(req,res)=>{
+    await servico.create(
+        req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: "Serviço criado com sucesso!"        
+        })
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro no cadastro do serviço"
+        })
+    });
+});
+
+app.post('/pedidos', async(req,res)=>{
     await pedido.create(
-        rep.body
-        // ClienteId: 1,
-        // dataPedido: "2022-16-02",
+        req.body
     ).then(function(){
         return res.json({
             error: false,
@@ -78,16 +69,11 @@ app.post('/pedidos', async(rep,res)=>{
             message: "Erro no cadastro do pedido"
         })
     });
-    // res.send('Pedido criado com sucesso!');
 })
 
-app.post('/itenspedido', async(rep,res)=>{
+app.post('/itenspedido', async(req,res)=>{
     await itempedido.create(
-        rep.body
-        // PedidoId: 1,
-        // ServicoId: 2,
-        // quantidade: 3,
-        // valor: 100.0
+        req.body
     ).then(function(){
         return res.json({
             error: false,
@@ -99,31 +85,59 @@ app.post('/itenspedido', async(rep,res)=>{
             message: "Erro no cadastro do item"
         })
     });
-    // res.send('Item criado com sucesso!');
 })
 
-// app.get('/clientes', function(rep,res){
-//     res.send('Sejam bem-vindos(as) a ServicesTI.')
-// });
+app.post('/compras', async(req,res)=>{
+    await compra.create(
+        req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: "Compra realizada com sucesso!"        
+        })
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro no cadastro da compra"
+        })
+    });
+});
 
-// app.get('/servicos', function(rep,res){
-//     res.send('E ae galerinha!!! Bora serviçar')
-// });
+app.post('/produtos', async(req,res)=>{
+    await produto.create(
+        req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: "Produto criado com sucesso!"        
+        })
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro no cadastro do produto"
+        })
+    });
+});
 
-// app.get('/pedidos', function(rep,res){
-//     res.send('Olha o seu pedido chegando!!!')
-// });
+app.post('/itenscompra', async(req,res)=>{
+    await itemcompra.create(
+        req.body
+    ).then(function(){
+        return res.json({
+            error: false,
+            message: "Item criado com sucesso!"        
+        })
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro no cadastro do item"
+        })
+    });
+})
 
 let port=process.env.PORT || 3001;
 
-app.get('/listaservicos', async(req,res)=>{
-    await servico.findAll({
-        raw: true
-        // order:[['nome','DESC']]
-    }).then(function(servicos){
-        res.json({servicos})
-    });
-});
+//listagem
 
 app.get('/listaclientes', async(req,res)=>{
     await cliente.findAll({
@@ -132,6 +146,15 @@ app.get('/listaclientes', async(req,res)=>{
     })
     .then(function(clientes){
         res.json({clientes})
+    });
+});
+
+app.get('/listaservicos', async(req,res)=>{
+    await servico.findAll({
+        raw: true
+        // order:[['nome','DESC']]
+    }).then(function(servicos){
+        res.json({servicos})
     });
 });
 
@@ -144,6 +167,42 @@ app.get('/listapedidos', async(req,res)=>{
         res.json({pedidos})
     });
 });
+
+app.get('/listaitenspedido', async(req,res)=>{
+    await itempedido.findAll({
+        raw: true
+    })
+    .then(function(itenspedido){
+        res.json({itenspedido})
+    });
+});
+
+app.get('/listaprodutos', async(req,res)=>{
+    await produto.findAll({
+        raw: true
+    }).then(function(produtos){
+        res.json({produtos})
+    });
+});
+
+app.get('/listacompras', async(req,res)=>{
+    await compra.findAll({
+        raw: true
+    })
+    .then(function(compras){
+        res.json({compras})
+    });
+});
+
+app.get('/listaitenscompra', async(req,res)=>{
+    await itemcompra.findAll({
+        raw: true
+    })
+    .then(function(itenscompra){
+        res.json({itenscompra})
+    });
+});
+
 
 app.get('/ofertaservicos', async(req,res)=>{
     await servico.count('id').then(function(servicos){
@@ -163,6 +222,13 @@ app.get('/nossospedidos', async(req,res)=>{
     });
 });
 
+app.get('/pedidos/:id', async(req,res)=>{
+    await pedido.findByPk(req.params.id,{include:[{all: true}]})
+    .then(ped =>{
+        return res.json({ped});
+    });
+});
+
 app.get('/servico/:id', async(req,res)=>{
     await servico.findByPk(req.params.id)
     .then(serv =>{
@@ -178,15 +244,40 @@ app.get('/servico/:id', async(req,res)=>{
     });
 });
 
-// app.get('/atualizaservico', async(req,res)=>{
-//     await servico.findByPk(1)
-//     .then(serv =>{
-//         serv.nome = 'HTML/CSS/JS';
-//         serv.descricao = 'Páginas estáticas e dinâmicas estelizadas';
-//         serv.save();
-//         return res.json({serv});
-//     });
-// });
+// atualizar e/ou editar
+
+app.put('/clientes/:id/editarcliente', async(req,res)=>{
+    const itemCliente = {
+        nome: req.body.nome,
+        endereco: req.body.endereco,
+        cidade: req.body.cidade,
+        uf: req.body.uf,
+        nascimento: req.body.nascimento,
+        clienteDesde: req.body.clienteDesde
+    };
+
+    if (!await cliente.findByPk(req.params.id)){
+        return res.status(400).json({
+            error: true,
+            message: "Cliente não encontrado."
+        });
+    };
+
+    await cliente.update(itemCliente,{
+        where: {id: req.body.id}
+    }).then(function(itens){
+        return res.json({
+            error: false,
+            message: "Cliente alterado com sucesso!",
+            itens
+        });
+    }).catch(function(error){
+        return res.status(400).json({
+            error: true,
+            message:"Erro: não foi possível fazer a alteração"
+        });
+    });
+});
 
 app.put('/atualizaservico', async(req,res)=>{
     await servico.update(req.body,{
@@ -201,13 +292,6 @@ app.put('/atualizaservico', async(req,res)=>{
             error: true,
             message: "Erro na alteração do serviço"
         });
-    });
-});
-
-app.get('/pedidos/:id', async(req,res)=>{
-    await pedido.findByPk(req.params.id,{include:[{all: true}]})
-    .then(ped =>{
-        return res.json({ped});
     });
 });
 
@@ -248,40 +332,7 @@ app.put('/pedidos/:id/editaritem', async(req,res)=>{
     });
 });
 
-app.put('/clientes/:id/editarcliente', async(req,res)=>{
-    const itemCliente = {
-        nome: req.body.nome,
-        endereco: req.body.endereco,
-        cidade: req.body.cidade,
-        uf: req.body.uf,
-        nascimento: req.body.nascimento,
-        clienteDesde: req.body.clienteDesde
-    };
-
-    if (!await cliente.findByPk(req.params.id)){
-        return res.status(400).json({
-            error: true,
-            message: "Cliente não encontrado."
-        });
-    };
-
-    await cliente.update(itemCliente,{
-        where: {id: req.body.id}
-    }).then(function(itens){
-        return res.json({
-            error: false,
-            message: "Cliente alterado com sucesso!",
-            itens
-        });
-    }).catch(function(error){
-        return res.status(400).json({
-            error: true,
-            message:"Erro: não foi possível fazer a alteração"
-        });
-    });
-});
-
-
+// exclusão
 
 app.get('/excluircliente/:id', async(req,res)=>{
     await cliente.destroy({
